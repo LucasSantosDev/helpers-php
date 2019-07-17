6741b9437ba5ee42b065a0348b8f2364c6ae4fa5
Everaldo Lima <jemlima@gmail.com>
2019-07-17 17:26:28 -0300

    Incluindo função de Sanitize.

--- src/MyHelpers.php
+++ src/MyHelpers.php
@@ -367,4 +367,18 @@ class MyHelpers
         header("Content-disposition: attachment; filename=$filename"); 
         echo $content;
     }
+
+    public static function sanitizeString($str)
+    {
+        $str = preg_replace('/[áàãâä]/ui', 'a', $str);
+        $str = preg_replace('/[éèêë]/ui', 'e', $str);
+        $str = preg_replace('/[íìîï]/ui', 'i', $str);
+        $str = preg_replace('/[óòõôö]/ui', 'o', $str);
+        $str = preg_replace('/[úùûü]/ui', 'u', $str);
+        $str = preg_replace('/[ç]/ui', 'c', $str);
+        // $str = preg_replace('/[,(),;:|!"#$%&/=?~^><ªº-]/', '_', $str);
+        $str = preg_replace('/[^a-z0-9]/i', '', $str);
+        $str = preg_replace('/_+/', '_', $str); // ideia do Bacco :)
+        return $str;
+    }
 }
